f5f9073396de4929d9cfea14cddb09c829bd0672
Timur Tursunov <tim.alex.turs@gmail.com>
2020-12-14 23:55:18 +0000

    here we go

--- conditionals.js
+++ conditionals.js
@@ -13,8 +13,7 @@ console.log('Now you\'re ready to go outside');
 
 const temperature = 32;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
+if (temperature < 0) {console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
 } else {
@@ -31,3 +30,10 @@ if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
