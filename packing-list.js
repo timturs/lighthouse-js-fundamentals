53d1e35736a4be1d89835c879f09c86731f707c6
Timur Tursunov <tim.alex.turs@gmail.com>
2020-12-16 01:09:43 +0000

    second take

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,11 @@ for (let i = 0; i < packingList; i++) {
   console.log(packingList[i]);
 
 }
+
+console.log('Kitchen stuff to pack:');
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++
+};
+
